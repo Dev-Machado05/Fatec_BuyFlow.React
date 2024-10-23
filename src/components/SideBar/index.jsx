8bdfb8193fb9b4b5b9cb45ca18343d1ea98ce1a6
Lucas Machado <lucasmachadoale4@gmail.com>
2024-10-23 19:18:42 -0300

    fix: fixed the redirect bug

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,15 +1,35 @@
 import "./style.css";
 
 export default function SideBar() {
-  return (
+    
+    function SwitchPage(pg) {
+        switch (pg) {
+            case 1:
+                window.location.href = "/";
+                break;
+            case 2:
+                window.location.href = "/Home";
+                break;
+            case 3:
+                window.location.href = "/Carrinho";
+                break;
+            case 4:
+                window.location.href = "/Sobre";
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
     <div className="SideBar">
       <div className="Navigation">
-        <button className="HomeButton">Home</button>
-        <button className="CardButton">Carrinho</button>
-        <button className="AboutButton">Sobre nós</button>
+        <button onClick={() => SwitchPage(2)}>Home</button>
+        <button onClick={() => SwitchPage(3)}>Carrinho</button>
+        <button onClick={() => SwitchPage(4)}>Sobre nós</button>
       </div>
 
-      <button className="LogoffButton">Desconectar</button>
+      <button className="LogoffButton" onClick={() => SwitchPage(1)}>Desconectar</button>
     </div>
   );
 }
